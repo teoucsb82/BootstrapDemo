c3f651d3e48244806a8bb47e26f2d777773de36a
= <=>
2014-03-13 14:10:58 -0700

    added success failure to contacts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,54 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+$(function() {
+  $('#contact-button').on('click', function(event) {
+    event.preventDefault();
+
+
+    var $fields = $(event.currentTarget).parent().children().children();
+    $fields.removeClass("alert alert-danger alert-success");
+
+    if ($('#contact-name').val() === "") {
+      $('#contact-name').addClass("alert alert-danger");
+    } else {
+      $('#contact-name').addClass("alert alert-success");
+    }
+
+    if ($('#contact-email').val() === "") {
+      $('#contact-email').addClass("alert alert-danger");
+    } else {
+      $('#contact-email').addClass("alert alert-success");
+    }
+
+    if ($('#contact-subject').val() === "") {
+      $('#contact-subject').addClass("alert alert-danger");
+    } else {
+      $('#contact-subject').addClass("alert alert-success");
+    }
+
+    if ($('#contact-message').val() === "") {
+      $('#contact-message').addClass("alert alert-danger");
+    } else {
+      $('#contact-message').addClass("alert alert-success");
+    }
+
+    if (!$fields.hasClass("alert-danger")) {
+      $('#contact-errors').removeClass("hidden")
+      $('#contact-errors').removeClass("alert-danger")
+      $('#contact-errors').addClass("alert-success")
+      $('#contact-error-strong').text("Success!")
+      $('#contact-error-text').text("Your message was sent")
+    } else {
+      $('#contact-errors').removeClass("hidden")
+      $('#contact-errors').removeClass("alert-success")
+      $('#contact-errors').addClass("alert-danger")
+      $('#contact-error-strong').text("Failure!")
+      $('#contact-error-text').text("Your message was not sent")
+    }
+
+
+
+  })
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130814223245) do
+ActiveRecord::Schema.define(version: 20140313185543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tickets", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.string   "subject",    null: false
+    t.text     "message",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username",        null: false
