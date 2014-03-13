4f100206fc114acbcfab0e48611db9afa13398d7
= <=>
2014-03-13 10:58:56 -0700

    added static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 NewAuthDemo::Application.routes.draw do
+  root :to => "static_pages#home"
+
+  get "static_pages/home"
+  get "static_pages/contact"
+  get "static_pages/about"
   resources :users, :only => [:create, :new, :show]
   resource :session, :only => [:create, :destroy, :new]
-
-  root :to => "users#show"
 end
